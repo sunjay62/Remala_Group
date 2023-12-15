59633195c1764f9d3b6b6ce30195f221206669a6
sunjay62 <sunannjaya@gmail.com>
2023-12-15 13:40:30 +0700

    update struktur organisasi

--- src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanEn.jsx
+++ src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanEn.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useTranslation } from 'react-i18next';
+import Chintia from '../../../../../assets/fotoprofile/chintia.png';
 
 const profiles = [
   {
     name: 'Chintia Hong',
-    image: '',
+    image: Chintia,
+    role: 'Corporate Secretary',
+    description:
+      'My name is Chintia Hong, In my current role at PT. Remala Abadi, where I have been serving since August 2022, I hold the position of Corporate Secretary since November 2023. In my earlier role from November 2022 to November 2023, I served as the Head of Customer Relation Department. Prior to this, I started as the Personal Assistant to the Board of Director from August 2022 to November 2022. In these capacities, I have been responsible for executing and maintaining all necessary official corporate filings, documents, reports, and records in compliance with applicable laws and regulations. This includes managing shareholder relations, communicating with board members, and coordinating shareholder lists and registrations. Additionally, I actively maintain communication with our customers through various channels, employing Likert Scale-based surveys to gauge satisfaction rates.<br><br>' +
+      'Previously, at XYNERGY REALTY INDONESIA, a Melbourne-based Property & Real Estate Company, I served as a Property Administrator Officer from February 2021 to August 2022. In this role, I processed renter applications, scheduled property inductions and open inspections through the IRE Application, sent welcome letters, and ensured accurate documentation in the database system. My responsibilities also involved close coordination with the Property Manager for application approvals and addressing any related issues.<br><br>' +
+      'Before that, at ASTON CIREBON HOTEL & CONVENTION CENTER in West Java, I held the position of Account Payable Clerk from October 2020 to February 2021. My responsibilities included receiving original invoices, preparing payment documents, and ensuring timely payments to suppliers, along with maintaining accurate records and filing.<br><br>' +
+      'In my role as a Purchasing Officer at SWISS-BELHOTEL JAYAPURA in Papua from August 2018 to July 2020, I managed the procurement process, negotiating prices, coordinating with users for specifications, and maintaining positive relationships with suppliers.<br><br>' +
+      'Earlier in my career, I played a key role as the Manager at CAFÉ CARSTENSZ in Jayapura from 2013 to 2017. In this venture, I successfully established and operated the café, overseeing various aspects such as finance, operations, maintenance, and human resources.<br><br>' +
+      'These diverse roles have equipped me with a comprehensive skill set, ranging from corporate governance and customer relations to property administration, accounts payable, and procurement, showcasing my ability to adapt and excel in various professional environments.',
   },
 ];
 
 const SekretarisPerusahaan = () => {
-  const { t } = useTranslation('global');
-
   useEffect(() => {
     AOS.init({
       duration: 1300,
@@ -26,8 +32,8 @@ const SekretarisPerusahaan = () => {
           <div className="col-6 profile-box">{profile.image && <img src={profile.image} alt={profile.name} />}</div>
           <div className="mx-4">
             <h6>{profile.name}</h6>
-            <p>{t(`translation.text-organization.secretary.role${index + 1}`)}</p>
-            <p>{t(`translation.text-organization.secretary.description${index + 1}`)}</p>
+            <p>{profile.role}</p>
+            <p dangerouslySetInnerHTML={{ __html: profile.description }} />
           </div>
         </div>
       ))}
--- src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanId.jsx
+++ src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanId.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useTranslation } from 'react-i18next';
+import Chintia from '../../../../../assets/fotoprofile/chintia.png';
 
 const profiles = [
   {
     name: 'Chintia Hong',
-    image: '',
+    image: Chintia,
+    role: 'Sekretaris Perusahaan',
+    description:
+      'Nama saya Chintia Hong, Dalam peran saya saat ini di PT. Remala Abadi, tempat saya menjabat sejak Agustus 2022, saya menjabat sebagai Sekretaris Perusahaan sejak November 2023. Pada jabatan saya sebelumnya dari November 2022 hingga November 2023, saya menjabat sebagai Kepala CRM ( Customer Relationship Management ). Sebelumnya, saya mulai sebagai Asisten Pribadi Dewan Direktur dari Agustus 2022 hingga November 2022. Dalam kapasitas ini, saya bertanggung jawab untuk melaksanakan dan memelihara semua pengarsipan, dokumen, laporan, dan catatan resmi perusahaan yang diperlukan sesuai dengan ketentuan yang berlaku undang-undang. Hal ini termasuk mengelola hubungan pemegang saham, berkomunikasi dengan anggota dewan, dan mengoordinasikan daftar dan pendaftaran pemegang saham. Selain itu, saya secara aktif menjaga komunikasi dengan pelanggan kami melalui berbagai saluran, menggunakan survei berbasis Skala Likert untuk mengukur tingkat kepuasan.<br><br>' +
+      'Sebelumnya, di XYNERGY REALTY INDONESIA, sebuah Perusahaan Properti & Real Estate yang berbasis di Melbourne, saya menjabat sebagai Administrator Properti dari Februari 2021 hingga Agustus 2022. Dalam peran ini, saya memproses aplikasi penyewa, induksi properti terjadwal, dan inspeksi terbuka melalui Aplikasi IRE , mengirimkan surat selamat datang, dan memastikan dokumentasi yang akurat dalam sistem database. Tanggung jawab saya juga mencakup koordinasi yang erat dengan Manajer Properti untuk persetujuan permohonan dan mengatasi masalah terkait.<br><br>' +
+      'Sebelumnya, di ASTON CIREBON HOTEL & CONVENTION CENTER di Jawa Barat, saya menjabat sebagai Account Payable Clerk dari Oktober 2020 hingga Februari 2021. Tanggung jawab saya termasuk menerima faktur asli, menyiapkan dokumen pembayaran, dan memastikan pembayaran tepat waktu kepada pemasok, serta memelihara pencatatan dan pengarsipan yang akurat.<br><br>' +
+      'Dalam peran saya sebagai Purchasing di SWISS-BELHOTEL JAYAPURA di Papua dari Agustus 2018 hingga Juli 2020, saya mengelola proses pengadaan, menegosiasikan harga, berkoordinasi dengan pengguna untuk spesifikasi, dan menjaga hubungan positif dengan pemasok.<br><br>' +
+      'Di awal karir saya, saya memegang peran penting sebagai Manajer di CAFÉ CARSTENSZ di Jayapura dari tahun 2013 hingga 2017. Dalam usaha ini, saya berhasil mendirikan dan mengoperasikan kafe, mengawasi berbagai aspek seperti keuangan, operasional, pemeliharaan, dan sumber daya manusia.<br><br>' +
+      'Peran yang beragam ini telah membekali saya dengan keahlian yang komprehensif, mulai dari tata kelola perusahaan dan hubungan pelanggan hingga administrasi properti, hutang, dan pengadaan, yang menunjukkan kemampuan saya untuk beradaptasi dan unggul dalam berbagai lingkungan profesional.',
   },
 ];
 
 const SekretarisPerusahaan = () => {
-  const { t } = useTranslation('global');
-
   useEffect(() => {
     AOS.init({
       duration: 1300,
@@ -26,8 +32,8 @@ const SekretarisPerusahaan = () => {
           <div className="col-6 profile-box">{profile.image && <img src={profile.image} alt={profile.name} />}</div>
           <div className="mx-4">
             <h6>{profile.name}</h6>
-            <p>{t(`translation.text-organization.secretary.role${index + 1}`)}</p>
-            <p>{t(`translation.text-organization.secretary.description${index + 1}`)}</p>
+            <p>{profile.role}</p>
+            <p dangerouslySetInnerHTML={{ __html: profile.description }} />
           </div>
         </div>
       ))}
