c5724016504a945fe3e2c287dc0dbd7d516ee678
sunjay62 <sunannjaya@gmail.com>
2024-01-29 12:18:35 +0700

    update meta data

--- src/components/home/HomeId.jsx
+++ src/components/home/HomeId.jsx
@@ -9,6 +9,8 @@ import PartnerId from './highlightpartner/PartnerId';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import TestimonialId from './highlighttestimonial/TestimonialId';
+import { Helmet } from 'react-helmet';
+import HomeImage from '../../assets/meta/homeimage.png';
 
 const HomeId = () => {
   useEffect(() => {
@@ -19,6 +21,13 @@ const HomeId = () => {
 
   return (
     <div className="homeMainContainer">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Remala Group - Home</title>
+        <link rel="canonical" href="https://remala.id/" />
+        <meta name="description" content="Welcome to Remala Group. We provide exceptional services in various fields." />
+        <meta name="image" content={HomeImage} />
+      </Helmet>
       <div className="content-title-home d-flex justify-content-center align-items-center" data-aos="fade-down">
         <div className="container">
           <h1 data-aos="fade-left">MEMBERIKAN KONEKTIVITAS TERBAIK</h1>
