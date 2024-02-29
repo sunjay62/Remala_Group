2d0cf9521ef2e8970dff015f91d2a55f4ee75fd4
sunjay62 <sunannjaya@gmail.com>
2024-02-29 12:25:24 +0700

    update swiper mobile

--- src/components/home/highlightswiper/SwiperId.jsx
+++ src/components/home/highlightswiper/SwiperId.jsx
@@ -31,7 +31,7 @@ const SimpleSlider = () => {
           }}
           navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper" // Menambahkan kelas CSS di sini
+          className="mySwiper"
         >
           {swiperDataId.map((slide) => (
             <SwiperSlide key={slide.id}>
