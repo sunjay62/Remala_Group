933ceca273cd5390cb5cb6ec71119473361c8a3e
sunjay62 <sunannjaya@gmail.com>
2024-04-03 16:20:32 +0700

    update foto profile secretary corporate

--- src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanEn.jsx
+++ src/components/about/menu/menu-struktur-organisasi/sekretarisperusahaan/SekretarisPerusahaanEn.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Maureen from '../../../../../assets/fotoprofile/maureen-new.png';
+import Maureen from '../../../../../assets/fotoprofile/maureen.png';
 import './sekretarisperusahaan.scss';
 
 const profiles = [
