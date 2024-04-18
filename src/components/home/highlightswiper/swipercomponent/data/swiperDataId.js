a964aecddd3c696c5c29cc7282ef1dae9881d35b
sunjay62 <sunannjaya@gmail.com>
2024-04-18 16:31:47 +0700

    update swiper

--- src/components/home/highlightswiper/swipercomponent/data/swiperDataId.js
+++ src/components/home/highlightswiper/swipercomponent/data/swiperDataId.js
@@ -9,21 +9,41 @@ const swiperData = [
     url: 'idm-event',
     image: require('../image/idm.jpg'),
   },
-  // {
-  //   id: 3,
-  //   url: 'title-1',
-  //   image: require('../image/bisnis4.jpg'),
-  // },
-  // {
-  //   id: 4,
-  //   url: 'title-1',
-  //   image: require('../image/bisnis5.jpg'),
-  // },
-  // {
-  //   id: 5,
-  //   url: 'title-1',
-  //   image: require('../image/bisnis3.jpg'),
-  // },
+  {
+    id: 3,
+    url: 'ims',
+    image: require('../image/iso/isoIMS.jpg'),
+  },
+  {
+    id: 4,
+    url: 'iso-9001:2015',
+    image: require('../image/iso/iso9001.jpg'),
+  },
+  {
+    id: 5,
+    url: 'iso-27001:2022',
+    image: require('../image/iso/iso27001.jpg'),
+  },
+  {
+    id: 6,
+    url: 'iso-37001:2016',
+    image: require('../image/iso/iso37001.jpg'),
+  },
+  {
+    id: 7,
+    url: 'iso-45001:2018',
+    image: require('../image/iso/iso45001.jpg'),
+  },
+  {
+    id: 8,
+    url: 'iso-14001:2015',
+    image: require('../image/iso/iso14001.jpg'),
+  },
+  {
+    id: 8,
+    url: 'iso-14001:2015',
+    image: require('../image/iso/iso14001.jpg'),
+  },
 ];
 
 export default swiperData;
